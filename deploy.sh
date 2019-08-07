5d38332cc0bd99de03287dc8d0a3092a3e13c72f
Benjamin Orthen <benjamin@orthen.net>
2019-08-07 12:56:28 +0200

    Revert hugo-goa theme to origin/master, add specific ssh identity for rsync

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 HOST=orthen.net
 DIR=../../web # might sometimes be empty!
 
-hugo && rsync -rlvz --delete --exclude=/.well-known --exclude=.htaccess public/ ${WEB_HOSTING_USER_ID}@${HOST}:~/${DIR}
+hugo && rsync -e "ssh -i $WEB_HOSTING_SSH_KEY_LOCATION" --numeric-ids -rlvz --delete --exclude=/.well-known --exclude=.htaccess public/ ${WEB_HOSTING_USER_ID}@${HOST}:~/${DIR}
 
 exit 0
