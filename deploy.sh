2baf094c9ff9f5c1b8e3923c80c22dca7fd380ca
Benjamin Orthen <benjamin@orthen.net>
2021-10-14 20:45:01 +0200

    remove unused image, use avif for profile pic, update page location

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-HOST=orthen.net
-DIR=../../web # might sometimes be empty!
+HOST=ngcobalt315.manitu.net
+DIR=../../web/homepage # might sometimes be empty!
 
 rsync -e "ssh -i $WEB_HOSTING_SSH_KEY_LOCATION" --numeric-ids -rlvz --delete --exclude=/.well-known --exclude=.htaccess public/ ${WEB_HOSTING_USER_ID}@${HOST}:~/${DIR}
 
